460901f1cbeeeae9848a1add81cd1ff3f6891769
sergey <serge.dmitriev@gmail.com>
2014-12-25 22:47:30 +0700

    rewrite to async work with json files

--- dataSource/file.js
+++ dataSource/file.js
@@ -2,59 +2,68 @@
 
 var fs = require('fs');
 
-var file = function(options){
+var file = function (options) {
 
     var getDataDirectory = function(){
         return options['dataDirectory'] || 'data';
-    }
+    };
 
     var checkStructure = function (structure) {
         return (structure['code'] && structure['code'].length === 3
             && structure['number'] && structure['number'].length === 7 );
-    }
+    };
 
-    var getFileNameByFirstDigit = function (digit) {        
-        var filename = '';
-        var files = fs.readdirSync(getDataDirectory());
-
-        files = files.filter(function (element) {
-            return ( element.lastIndexOf('json') != -1
-                && element.lastIndexOf(digit) != -1 )
+    var getFileNameByFirstDigit = function (digit, callback) {
+        fs.readdir(getDataDirectory(), function (err, files) {
+            files = files.filter(function (element) {
+                return ( element.lastIndexOf('json') != -1
+                    && element.lastIndexOf(digit) != -1 )
+                });
+            callback(files[0]);
         });
-        
-        return files[0];
-    }
+    };
 
-    var getFilePath = function (structure) {
-        var filename = getFileNameByFirstDigit(structure.code.charAt(0));
-        if (!filename) throw new Error('Check files with data');
+    var getFilePath = function (structure, callback) {
+        getFileNameByFirstDigit(structure.code.charAt(0), function (filename) {
+            if (!filename) throw new Error('Check files with data');
+            callback(getDataDirectory() + '/' + filename);
+        });
+    };
 
-        return getDataDirectory() + '/' + filename;
-    }
-
-    var getData = function (structure) {        
+    var getData = function (structure, callback) {
         var array,
             encoding = 'utf8';
 
-        if (checkStructure(structure)) {    
-            array = JSON.parse(fs.readFileSync(getFilePath(structure), encoding));
+        if (!checkStructure(structure)) throw new Error('Check structure of number');
+        
+        getFilePath(structure, function (filepath) {
+            fs.readFile(filepath, encoding, function (err, data) {
+                if (err) throw err;
+                array = JSON.parse(data);
 
-            array = array.filter(function (element) {
-                return (element['code'] === structure['code'] 
-                        && (element['beginNumber'] <= parseInt(structure['number']) 
-                            && element['endNumber'] >= parseInt(structure['number']) )
-                        )
+                array = array.filter(function (element) {
+                    return (element['code'] === structure['code'] 
+                            && (element['beginNumber'] <= parseInt(structure['number']) 
+                                && element['endNumber'] >= parseInt(structure['number']) )
+                            )
+                });
+
+                callback(null, array[0]);
             });
-        } else {
-            throw new Error('Check structure of number');
-        }
-
-        return array[0];
-    }
+        });        
+    };
 
     return {
         getData: getData
     }
 }
+
+
+var q = new file({dataDirectory: '../data'});
+
+q.getData({code:'391', number: '2148000'}, function (err,data) {
+    console.log(err, data);
+});
+
 
 module.exports = file;
