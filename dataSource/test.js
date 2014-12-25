94e98cb6a2b5e824412c45183a0cdd73adbdc768
sergey <serge.dmitriev@gmail.com>
2014-12-25 23:48:02 +0700

    add mongo finder

--- dataSource/test.js
+++ dataSource/test.js
@@ -1,9 +1,16 @@
 
 var file = require('./file');
+var mongo = require('./mongo');
 
-var q = new file({dataDirectory: '../data'});
+var q1 = new file({dataDirectory: '../data'});
 
-q.getData({code: '391', number: '2148000'}, function (err,data) {
+q1.getData({code: '391', number: '2148000'}, function (err, data) {
     console.log(err, data);
 });
 
+
+var q2 = new mongo({host: 'localhost', port: 27017, db: 'capacity', collection: 'capacity'});
+
+q2.getData({code: "391", number: "2148000"}, function (err, data) {
+    console.log(err, data);
+});
