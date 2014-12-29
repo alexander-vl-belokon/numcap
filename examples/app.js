4186368f89013327f1c43372fc03c8e24ab11c0e
Sergey Dmitriev <serge.dmitriev@gmail.com>
2014-12-29 15:40:17 +0700

    update examples

--- examples/app.js
+++ examples/app.js
@@ -1,10 +1,14 @@
 var numcap = require('../index');
 
-var q3 = new numcap({type: 'file'});
 
-q3.getData("56576567", function (err, data) {
+var q2 = new numcap({type: 'file'});
+
+q2.getData("8-391-2745000", function (err, data) {
     console.log(err, data);
 });
+
+
+var q3 = new numcap({type: 'mongo', options: { collection: 'capacity4'}});
 
 q3.getData("8-391-2745000", function (err, data) {
     console.log(err, data);
