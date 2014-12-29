50b21d0271ddd1257344f2709b9fa920d7479885
Sergey Dmitriev <serge.dmitriev@gmail.com>
2014-12-29 12:13:31 +0700

    change example

--- examples/app.js
+++ examples/app.js
@@ -1,12 +1,11 @@
 var numcap = require('../index');
-var number = '+79131775501';
 
-var q3 = new numcap({type: 'file', options: {dataDirectory: '../data'}});
+var q3 = new numcap({type: 'file'});
 
 q3.getData("56576567", function (err, data) {
     console.log(err, data);
 });
 
-q3.getData("8-913-529-29-26", function (err, data) {
+q3.getData("8-391-2745000", function (err, data) {
     console.log(err, data);
 });
